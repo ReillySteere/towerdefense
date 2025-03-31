b23af1f3c0f5d662f5d62bab0f4f7648361c52fa
Reilly Goulding <reilly.goulding@gettyimages.com>
2025-03-30 21:16:31 -0600

    Enable SASS support and import styles as modules with Webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
+import styles from './styles.module.scss';
 import './phaserGame';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className={styles.container}>
+      <div id="phaser-container" className={styles.phaserContainer}></div>
+      <div id="root-ui" className={styles.uiOverlay}>
+        <h1>React UI Overlay</h1>
+        {/* Additional UI components */}
+      </div>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <App />
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.module.scss'],
   },
   module: {
     rules: [
@@ -16,6 +16,31 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+              esModule: false,
+              modules: {
+                auto: /\.module\.[a-z]+$/,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sourceMap: true,
+            },
+          },
+        ],
       },
     ],
   },
