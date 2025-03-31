e416faeb8c10d1bd4ef9c3ebd4053020ee997ec9
Reilly Goulding <reilly.goulding@gettyimages.com>
2025-03-30 22:31:20 -0600

    File updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import './phaserGame';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/browser';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './react/App';
+
+Sentry.init({
+  dsn: 'https://349f5174f58c6bcd4b3b5fb5fb738ff3@o4509070478147584.ingest.de.sentry.io/4509070482210896', // Replace with your Sentry DSN
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+});
 
 const queryClient = new QueryClient();
 
